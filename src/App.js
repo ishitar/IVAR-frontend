5457a4a503bb9d7540ce7c7df70c992aa1f76320
Varun Gupta <varun.gupta3@honeywell.com>
2020-04-14 01:49:21 +0530

    Added a dummy landing page and buttons to move to login and signup page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import Login from "./components/login/login.component";
 import Register from "./components/register/register.component";
+import LandingPage from "./pages/landing/landing.component";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        component={LandingPage}
+                    />
                     <Route
                         exact
                         path="/login"
