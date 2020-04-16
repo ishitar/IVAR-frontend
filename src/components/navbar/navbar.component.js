b6dfe8840783614fbf7d50b410283a59985ce5aa
Varun Gupta <varun.gupta3@honeywell.com>
2020-04-16 13:05:36 +0530

    Added box shadow to sidebar, added text shadow to logo, changed the font to barlow

--- src/components/navbar/navbar.component.js
+++ src/components/navbar/navbar.component.js
@@ -1,54 +1,53 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Navbar = ({loggedIn}) => (
-    <div>
-        {
-            loggedIn ?
-                <NavWrapper className="nav flex-column justify-content-center">
-                    <Link to='/timeline' className="nav-item">
-                        <i className="fa fa-feed"/>
-                    </Link>
-                    <Link to='/profile' className="nav-item">
-                        <i className="fa fa-user-o"/>
-                    </Link>
-                    <Link to='/explore' className="nav-item">
-                        <i className="fa fa-search"/>
-                    </Link>
-                    <Link to='/chat' className="nav-item">
-                        <i className="fa fa-comments-o"/>
-                    </Link>
-                    <Link to='/notifications' className="nav-item">
-                        <i className="fa fa-bell-o"/>
-                    </Link>
-                    <Link to='/post' className="nav-item">
-                        <i className="fa fa-edit"/>
-                    </Link>
-                </NavWrapper> : null
-        }
-    </div>
-
+const Navbar = ({ loggedIn }) => (
+	<div>
+		{loggedIn ? (
+			<NavWrapper className="nav flex-column justify-content-center">
+				<Link to="/timeline" className="nav-item">
+					<i className="fa fa-feed" />
+				</Link>
+				<Link to="/profile" className="nav-item">
+					<i className="fa fa-user-o" />
+				</Link>
+				<Link to="/explore" className="nav-item">
+					<i className="fa fa-search" />
+				</Link>
+				<Link to="/chat" className="nav-item">
+					<i className="fa fa-comments-o" />
+				</Link>
+				<Link to="/notifications" className="nav-item">
+					<i className="fa fa-bell-o" />
+				</Link>
+				<Link to="/post" className="nav-item">
+					<i className="fa fa-edit" />
+				</Link>
+			</NavWrapper>
+		) : null}
+	</div>
 );
 
 const NavWrapper = styled.div`
-margin-top: 5px 0 5px 5px;
-min-height:100vh;
-display:inline-flex;
-width:70px;
-justify-content: center;
-text-align: center;
-position: fixed;
-.nav-item{
-    color:var(--mainWhite); 
-    margin-top:10px;
-}
-.nav-item:hover{
-    text-shadow:0px 0px 7px var(--lightGrey);
-}
-.fa{
-    font-size:1.5rem;
-}
+	margin-top: 5px 0 5px 5px;
+	box-shadow: 6px 5px 5px 1px rgba(31, 31, 31, 1);
+	min-height: 100vh;
+	display: inline-flex;
+	width: 75px;
+	justify-content: center;
+	text-align: center;
+	position: fixed;
+	.nav-item {
+		color: var(--mainWhite);
+		margin-top: 10px;
+	}
+	.nav-item:hover {
+		text-shadow: 0px 0px 7px var(--lightGrey);
+	}
+	.fa {
+		font-size: 1.5rem;
+	}
 `;
 
 export default Navbar;
